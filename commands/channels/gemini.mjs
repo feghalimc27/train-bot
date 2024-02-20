ad8a21d91c7ffa5f4486fc7acf5019d48412aa7f
AlexanderHamlet <alexander.m.hamlet@gmail.com>
2024-02-19 21:17:41 -0700

    Return embedded response.

--- commands/channels/gemini.mjs
+++ commands/channels/gemini.mjs
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from 'discord.js';
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
 import { GoogleAuth } from 'google-auth-library';
 import { VertexAI } from '@google-cloud/vertexai';
 
@@ -8,6 +8,7 @@ const defaultModel = process.env.GEMINI_MODEL_DEFAULT;
 const maxPromptLength = Number(process.env.GEMINI_MAX_PROMPT_LENGTH);
 const maxOutputTokens = Number(process.env.GEMINI_MAX_OUTPUT_TOKENS);
 
+var geminiChatId = null;
 var geminiChatSession = null;
 
 export const command = {
@@ -39,6 +40,7 @@ export const command = {
 
         await interaction.deferReply();
         if (geminiChatSession === null || newChat) {
+            geminiChatId = genPseudoRandomString(10);
             geminiChatSession = startChat(model);
         }
 
@@ -49,7 +51,16 @@ export const command = {
             content = content.concat('\n', part.text);
         }
         console.log('Content Object: ' + JSON.stringify(content));
-        await interaction.editReply(content);
+        embedResponse = new EmbedBuilder()
+            .setColor(0x0099FF)
+            .addFields(
+                {name: `${interaction.user.username}`, value: prompt},
+                {name: 'Gemini', value: content}
+            )
+            .setFooter({text: `Chat ID: ${geminiChatId}`})
+
+
+        await interaction.editReply(embedResponse);
     },
 }
 
@@ -68,3 +79,15 @@ const startChat = function(modelName) {
     });
     return generativeModel.startChat({});
 };
+
+const genPseudoRandomString = function(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        counter += 1;
+    }
+    return result;
+}
